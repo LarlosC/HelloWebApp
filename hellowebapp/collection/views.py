f3561db8f4ea0309c259ea435fb9a522d606f32a
Carlos <copez.larlos@gmail.com>
2016-03-21 22:08:45 -0700

    Updated model titles, added queryset to views.py

--- hellowebapp/collection/views.py
+++ hellowebapp/collection/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from collection.models import Thing
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    things = Thing.objects.all()
+    return render(request, 'index.html', {
+        'things': things,
+    })
